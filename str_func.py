e24b8f3a2d26986495a3a544f83181838193a330
Alexey Dyakonov <dyakonov261@gmail.com>
2024-05-28 22:29:00 +0500

    Откорректировал функцию

--- str_func.py
+++ str_func.py
@@ -2,6 +2,6 @@ def str_func(a):
     """"Перед строки в заглавные буквы"""
     return a.upper()
 
-def str_func(a):
+def str_fun(a):
     """"Первая буква становится заглавной"""
     return a.title()
